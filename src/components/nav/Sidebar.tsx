3e0de2657372ac47a7a0025fc5750360c34474a6
Dennis <dennis.badagliacca444@gmail.com>
2024-05-07 23:35:24 +0200

    graphics fix

--- src/components/nav/Sidebar.tsx
+++ src/components/nav/Sidebar.tsx
@@ -10,26 +10,26 @@ type Props = {}
 const Sidebar = (props: Props) => {
     const pathName = usePathname();
     return (
-        <div className="drawer z-50">
+        <div className="drawer z-50 absolute">
             <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col">
                 {/* Navbar */}
                 <div className="w-full navbar bg-base-200">
-                    <div className="flex-none lg:hidden">
-                        <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                        </label>
-                    </div>
-                    <div className="flex-1 items-center">
-                        <Link href={"/orders"} className="btn btn-ghost text-xl rounded-lg">Luna  <Image
-                            className=' rounded-xl'
-                            src={logo}
-                            width={40}
-                            height={40}
-                            alt="Picture of the author"
-                        /></Link>
-
-                    </div>
+                    {pathName === "/" ? "" :
+                        <div className="flex-none lg:hidden">
+                            <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                            </label>
+                        </div>
+                    }
+                    {pathName === "/" ?
+                        <div className="flex-1 items-center">
+                            <button className="btn btn-ghost text-xl rounded-lg">Luna</button>
+                        </div> :
+                        <div className="flex-1 items-center">
+                            <Link href={"/orders"} className="btn btn-ghost text-xl rounded-lg">Luna</Link>
+                        </div>
+                    }
                     {pathName === "/" ? "" :
                         <div className="flex-none hidden lg:block">
                             <ul className="menu menu-horizontal">
