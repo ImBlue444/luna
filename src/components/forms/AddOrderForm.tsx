da9e2002e91bdef4a49e9dec2a2c030e09b620a4
Dennis <dennis.badagliacca444@gmail.com>
2024-06-26 19:32:42 +0200

    refactor: Update modal text in Order.tsx and AddOrderForm.tsx

--- src/components/forms/AddOrderForm.tsx
+++ src/components/forms/AddOrderForm.tsx
@@ -249,7 +249,7 @@ const AddOrderForm = (props: Props) => {
             {/* //Modal */}
             <dialog id="my_modal_1" className="modal">
                 <div className="modal-box">
-                    <h3 className="font-bold text-lg">Hello!</h3>
+                    <h3 className="font-bold text-lg">Attenzione!</h3>
                     <p className="py-4">Sicuro di voler eliminare la commessa?</p>
                     <div className="modal-action">
                         <form method="dialog">
